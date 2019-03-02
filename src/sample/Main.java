80a7f7dc6162fdd9136da004c2392439cddabce5
Bartosz Górka <bartosz.gorka@interia.eu>
2019-03-02 10:28:09 +0100

    chore: Reorganize project's structure. Split classes and responsibilities. (#2)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,6 +3,8 @@ package sample;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -10,9 +12,14 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        InputInstance inputInstance = new InputInstance();
-        inputInstance.initInputInstance("objects.data");
+    public void start(Stage primaryStage) {
+        Reader reader = new Reader();
+        ArrayList<PointCoordinates> coordinates = reader.readInstance("objects.data");
+
+        EuclideanDistance euclideanDistance = new EuclideanDistance();
+        double[][] distanceMatrix = euclideanDistance.calculateDistanceMatrix(coordinates);
+
+        InputInstance inputInstance = new InputInstance(distanceMatrix);
         inputInstance.setGroupNumber(10);
 
         new Drawer().drawInputInstance(inputInstance, primaryStage);
